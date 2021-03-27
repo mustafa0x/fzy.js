24d7ea9ef2f5f8e19739039eeea2a3aa99c0a2cf
Mustafa J <mustafa.0x@gmail.com>
2021-03-27 13:44:44 -0700

    minor cleanups

--- index.js
+++ index.js
@@ -11,168 +11,168 @@ var SCORE_MATCH_CAPITAL = 0.7
 var SCORE_MATCH_DOT = 0.6
 
 function islower(s) {
-	return s.toLowerCase() === s;
+    return s.toLowerCase() === s;
 }
 
 function isupper(s) {
-	return s.toUpperCase() === s;
+    return s.toUpperCase() === s;
 }
 
 function precompute_bonus(haystack) {
-	/* Which positions are beginning of words */
-	var m = haystack.length;
-	var match_bonus = new Array(m);
+    /* Which positions are beginning of words */
+    var m = haystack.length;
+    var match_bonus = new Array(m);
 
-	var last_ch = '/';
-	for (var i = 0; i < m; i++) {
-		var ch = haystack[i];
+    var last_ch = '/';
+    for (var i = 0; i < m; i++) {
+        var ch = haystack[i];
 
-		if (last_ch === '/') {
-			match_bonus[i] = SCORE_MATCH_SLASH;
-		} else if (last_ch === '-' || last_ch === '_' || last_ch === ' ') {
-			match_bonus[i] = SCORE_MATCH_WORD;
-		} else if (last_ch === '.') {
-			match_bonus[i] = SCORE_MATCH_DOT;
-		} else if (islower(last_ch) && isupper(ch)) {
-			match_bonus[i] = SCORE_MATCH_CAPITAL;
-		} else {
-			match_bonus[i] = 0;
-		}
+        if (last_ch === '/') {
+            match_bonus[i] = SCORE_MATCH_SLASH;
+        } else if (last_ch === '-' || last_ch === '_' || last_ch === ' ') {
+            match_bonus[i] = SCORE_MATCH_WORD;
+        } else if (last_ch === '.') {
+            match_bonus[i] = SCORE_MATCH_DOT;
+        } else if (islower(last_ch) && isupper(ch)) {
+            match_bonus[i] = SCORE_MATCH_CAPITAL;
+        } else {
+            match_bonus[i] = 0;
+        }
 
-		last_ch = ch;
-	}
+        last_ch = ch;
+    }
 
-	return match_bonus;
+    return match_bonus;
 }
 
 function compute(needle, haystack, D, M) {
-	var n = needle.length;
-	var m = haystack.length;
+    var n = needle.length;
+    var m = haystack.length;
 
-	var lower_needle = needle.toLowerCase();
-	var lower_haystack = haystack.toLowerCase();
+    var lower_needle = needle.toLowerCase();
+    var lower_haystack = haystack.toLowerCase();
 
-	var match_bonus = precompute_bonus(haystack, match_bonus);
+    var match_bonus = precompute_bonus(haystack, match_bonus);
 
-	/*
-	 * D[][] Stores the best score for this position ending with a match.
-	 * M[][] Stores the best possible score at this position.
-	 */
+    /*
+     * D[][] Stores the best score for this position ending with a match.
+     * M[][] Stores the best possible score at this position.
+     */
 
-	for (var i = 0; i < n; i++) {
-		D[i] = new Array(m);
-		M[i] = new Array(m);
+    for (var i = 0; i < n; i++) {
+        D[i] = new Array(m);
+        M[i] = new Array(m);
 
-		var prev_score = SCORE_MIN;
-		var gap_score = i === n - 1 ? SCORE_GAP_TRAILING : SCORE_GAP_INNER;
+        var prev_score = SCORE_MIN;
+        var gap_score = i === n - 1 ? SCORE_GAP_TRAILING : SCORE_GAP_INNER;
 
-		for (var j = 0; j < m; j++) {
-			if (lower_needle[i] === lower_haystack[j]) {
-				var score = SCORE_MIN;
-				if (!i) {
-					score = (j * SCORE_GAP_LEADING) + match_bonus[j];
-				} else if (j) { /* i > 0 && j > 0*/
-					score = Math.max(
-						M[i - 1][j - 1] + match_bonus[j],
+        for (var j = 0; j < m; j++) {
+            if (lower_needle[i] === lower_haystack[j]) {
+                var score = SCORE_MIN;
+                if (!i) {
+                    score = (j * SCORE_GAP_LEADING) + match_bonus[j];
+                } else if (j) { /* i > 0 && j > 0*/
+                    score = Math.max(
+                        M[i - 1][j - 1] + match_bonus[j],
 
-						/* consecutive match, doesn't stack with match_bonus */
-						D[i - 1][j - 1] + SCORE_MATCH_CONSECUTIVE);
-				}
-				D[i][j] = score;
-				M[i][j] = prev_score = Math.max(score, prev_score + gap_score);
-			} else {
-				D[i][j] = SCORE_MIN;
-				M[i][j] = prev_score = prev_score + gap_score;
-			}
-		}
-	}
+                        /* consecutive match, doesn't stack with match_bonus */
+                        D[i - 1][j - 1] + SCORE_MATCH_CONSECUTIVE);
+                }
+                D[i][j] = score;
+                M[i][j] = prev_score = Math.max(score, prev_score + gap_score);
+            } else {
+                D[i][j] = SCORE_MIN;
+                M[i][j] = prev_score = prev_score + gap_score;
+            }
+        }
+    }
 }
 
 function score(needle, haystack) {
-	var n = needle.length;
-	var m = haystack.length;
+    var n = needle.length;
+    var m = haystack.length;
 
-	if (!n || !m)
-		return SCORE_MIN;
+    if (!n || !m)
+        return SCORE_MIN;
 
-	if (n === m) {
-		/* Since this method can only be called with a haystack which
-		 * matches needle. If the lengths of the strings are equal the
-		 * strings themselves must also be equal (ignoring case).
-		 */
-		return SCORE_MAX;
-	}
+    if (n === m) {
+        /* Since this method can only be called with a haystack which
+         * matches needle. If the lengths of the strings are equal the
+         * strings themselves must also be equal (ignoring case).
+         */
+        return SCORE_MAX;
+    }
 
-	if (m > 1024) {
-		/*
-		 * Unreasonably large candidate: return no score
-		 * If it is a valid match it will still be returned, it will
-		 * just be ranked below any reasonably sized candidates
-		 */
-		return SCORE_MIN;
-	}
+    if (m > 1024) {
+        /*
+         * Unreasonably large candidate: return no score
+         * If it is a valid match it will still be returned, it will
+         * just be ranked below any reasonably sized candidates
+         */
+        return SCORE_MIN;
+    }
 
-	var D = new Array(n);
-	var M = new Array(n);
+    var D = new Array(n);
+    var M = new Array(n);
 
-	compute(needle, haystack, D, M)
+    compute(needle, haystack, D, M)
 
-	return M[n - 1][m - 1];
+    return M[n - 1][m - 1];
 }
 
 function positions(needle, haystack) {
-	var n = needle.length;
-	var m = haystack.length;
+    var n = needle.length;
+    var m = haystack.length;
 
-	var positions = new Array(n);
+    var positions = new Array(n);
 
-	if (!n || !m)
-		return positions;
+    if (!n || !m)
+        return positions;
 
-	if (n === m) {
-		for (var i = 0; i < n; i++)
-			positions[i] = i;
-		return positions;
-	}
+    if (n === m) {
+        for (var i = 0; i < n; i++)
+            positions[i] = i;
+        return positions;
+    }
 
-	if (m > 1024) {
-		return positions;
-	}
+    if (m > 1024) {
+        return positions;
+    }
 
-	var D = new Array(n);
-	var M = new Array(n);
+    var D = new Array(n);
+    var M = new Array(n);
 
-	compute(needle, haystack, D, M)
+    compute(needle, haystack, D, M)
 
-	/* backtrack to find the positions of optimal matching */
-	var match_required = false;
+    /* backtrack to find the positions of optimal matching */
+    var match_required = false;
 
-	for (var i = n - 1, j = m - 1; i >= 0; i--) {
-		for (; j >= 0; j--) {
-			/*
-			 * There may be multiple paths which result in
-			 * the optimal weight.
-			 *
-			 * For simplicity, we will pick the first one
-			 * we encounter, the latest in the candidate
-			 * string.
-			 */
-			if (D[i][j] !== SCORE_MIN &&
-			    (match_required || D[i][j] === M[i][j])) {
-				/* If this score was determined using
-				 * SCORE_MATCH_CONSECUTIVE, the
-				 * previous character MUST be a match
-				 */
-				match_required =
-				    i && j &&
-				    M[i][j] === D[i - 1][j - 1] + SCORE_MATCH_CONSECUTIVE;
-				positions[i] = j--;
-				break;
-			}
-		}
-	}
+    for (var i = n - 1, j = m - 1; i >= 0; i--) {
+        for (; j >= 0; j--) {
+            /*
+             * There may be multiple paths which result in
+             * the optimal weight.
+             *
+             * For simplicity, we will pick the first one
+             * we encounter, the latest in the candidate
+             * string.
+             */
+            if (D[i][j] !== SCORE_MIN &&
+                (match_required || D[i][j] === M[i][j])) {
+                /* If this score was determined using
+                 * SCORE_MATCH_CONSECUTIVE, the
+                 * previous character MUST be a match
+                 */
+                match_required =
+                    i && j &&
+                    M[i][j] === D[i - 1][j - 1] + SCORE_MATCH_CONSECUTIVE;
+                positions[i] = j--;
+                break;
+            }
+        }
+    }
 
-	return positions;
+    return positions;
 }
 
 function hasMatch(needle, haystack) {
@@ -187,21 +187,21 @@ function hasMatch(needle, haystack) {
 }
 
 export {
-	/* constants */
-	SCORE_MIN,
-	SCORE_MAX,
+    /* constants */
+    SCORE_MIN,
+    SCORE_MAX,
 
-	SCORE_GAP_LEADING,
-	SCORE_GAP_TRAILING,
-	SCORE_GAP_INNER,
-	SCORE_MATCH_CONSECUTIVE,
-	SCORE_MATCH_SLASH,
-	SCORE_MATCH_WORD,
-	SCORE_MATCH_CAPITAL,
-	SCORE_MATCH_DOT,
+    SCORE_GAP_LEADING,
+    SCORE_GAP_TRAILING,
+    SCORE_GAP_INNER,
+    SCORE_MATCH_CONSECUTIVE,
+    SCORE_MATCH_SLASH,
+    SCORE_MATCH_WORD,
+    SCORE_MATCH_CAPITAL,
+    SCORE_MATCH_DOT,
 
-	/* functions */
-	score,
-	positions,
-	hasMatch
+    /* functions */
+    score,
+    positions,
+    hasMatch
 }
